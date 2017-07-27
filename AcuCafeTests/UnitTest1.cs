ca243bcd85b3dfc948254bcc7b0dd4cb73299ac3
Baffour <Baffour@users.noreply.github.com>
2017-07-27 22:32:57 +0100

    Add Tests for expected cost

--- AcuCafeTests/UnitTest1.cs
+++ AcuCafeTests/UnitTest1.cs
@@ -7,9 +7,18 @@ namespace AcuCafeTests
     [TestFixture]
     public class UnitTest1
     {
-        [TestCase]
-        public void TestMethod1()
+        [TestCase("Expresso", false, false, 1.8)]
+        [TestCase("Expresso", false, true, 2.3)]
+        [TestCase("Expresso", true, true, 2.8)]
+        [TestCase("HotTea", false, false, 1)]
+        [TestCase("HotTea", false, true, 1.5)]
+        [TestCase("HotTea", true, true, 2.0)]
+        [TestCase("IceTea", false, false, 1.5)]
+        [TestCase("IceTea", false, true, 2.0)]
+        public void TestCosts(string type, bool hasMilk, bool hasSugar,double expectedCost)
         {
+            var drink = AcuCafe.AcuCafe.OrderDrink(type, hasMilk, hasSugar);
+            Assert.AreEqual(expectedCost, drink.Cost());
         }
     }
 }
