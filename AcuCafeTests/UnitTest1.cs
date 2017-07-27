96588bdb94f90c3d08aeed067ede158ed3a94daa
Baffour <Baffour@users.noreply.github.com>
2017-07-27 22:12:56 +0100

    Add NUnit Test Project

---
+++ AcuCafeTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using AcuCafe;
+using NUnit.Framework;
+using System;
+
+namespace AcuCafeTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+        }
+    }
+}
